5432b7c139115bdee35134b74254d8db9881d209
Carly Russell <c.a.movius@gmail.com>
2019-07-03 16:52:41 -0700

    performing a smaller radius search

---
+++ api_keys1.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+api_key_openweather = "d428b3d15bbd55fa725b760b6719de9c"
+
+yelp_carly_api = "xd2SNzqqJzdLbEq_PaLOrIg8X7ExYGvchzMSgHxV1_MzmCK-9eCg1DnBEz8epku-GUjgs3yDQ0vhgR1WVphaDxwvI7m-_1gm0Ixrt6jcnXi2eFsHDRzS4RUhbp0bXXYx"
+
+yelp_loba_api= "3i2wMSEoNS35OGY8CACr-Pi99UVzYxaMxaxiBz3w8ya59m9bMXDCgOh1WUB2xJ_sRUGFwA837a9aLR473YH5Q3jVQTAtWke2uVx-y7EVxa565ZLdHvgqh1E3xqobXXYx"
